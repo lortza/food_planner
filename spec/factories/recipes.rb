deb9c2d5f711851a470d53a05c70bdfeca5adbde
anne <richardson.ae@gmail.com>
2018-08-27 20:48:17 -0500

    Corrects rubocop suggestions

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  recipe_instructions = [
+    'Lorem ipsum dolor sit amet.',
+    'Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.',
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+    'Nisi ut aliquip ex ea commodo consequat.',
+    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    'Excepteur sint occaecat cupidatat non proident.',
+    'Sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  ]
   factory :recipe do
     sequence(:title) { |n| "Recipe Title #{n}" }
     sequence(:source_name) { |n| "Recipe Source #{n}" }
     sequence(:source_url) { |n| "http://recipesource#{n}.com" }
     servings { rand(10) }
-    instructions { 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' }
+    instructions recipe_instructions.join("\n\n")
   end
 end
