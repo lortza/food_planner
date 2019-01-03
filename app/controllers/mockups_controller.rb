9cc0754415672d278ef464f56d402b8a2bc1290c
Anne Richardson <richardson.ae@gmail.com>
2019-01-02 19:40:52 -0600

    Add column mockup

--- app/controllers/mockups_controller.rb
+++ app/controllers/mockups_controller.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class MockupsController < ApplicationController
+  # These are temporary pages to help me figure out what the UX should be.
+  # Since I'm not sure how I want to use the app, I'm not sure how to
+  # build the back end.
+
   def recipes
     # randomly generated temporaty placeholder recipes
     @recipes = Recipe.by_title
     @prep_plan_recipes = Recipe.all.sample(5)
+    @upcoming_prep_plan = Preparation.next_upcoming
   end
+
+  # def shopping_lists
+  # end
+
+  # def add_to_prep_plan
+  # end
 end
