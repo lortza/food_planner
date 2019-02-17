13c16f87287324e7f223516f4345fa8fe2f03ec1
Anne Richardson <richardson.ae@gmail.com>
2019-02-17 12:32:40 -0600

    Build out crud for Meal Plan forms

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MealPlansController < ApplicationController
-  before_action :set_meal_plan, only: [:show]
+  before_action :set_meal_plan, only: [:show, :edit, :update, :destroy]
 
   def index
     @meal_plans = MealPlan.includes(:recipes).most_recent_first
@@ -11,13 +11,41 @@ class MealPlansController < ApplicationController
     @ingredient_set = IngredientSet.build_set(@meal_plan)
   end
 
+  def new
+    default_params = {
+      people_served: 2,
+      start_date: MealPlan.date_for_upcoming_sunday
+    }
+    @meal_plan = MealPlan.new(default_params)
+  end
+
+  def create
+    @meal_plan = MealPlan.new(meal_plan_params)
+    if @meal_plan.save
+      redirect_to meal_plan_url(@meal_plan)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @meal_plan.update(meal_plan_params)
+      redirect_to meal_plan_url(@meal_plan), notice: 'Meal Plan Updated'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_meal_plan
     @meal_plan = MealPlan.find(params[:id])
   end
 
-  def meail_plan_params
-    params.require(:meal_plan).permit(:start_date, :people_served)
+  def meal_plan_params
+    params.require(:meal_plan).permit(:start_date, :people_served, recipe_ids: [])
   end
 end
