6a3e17987dcf5700b9c85df9961474104f3a9d4a
Anne Richardson <richardson.ae@gmail.com>
2019-02-24 18:31:45 -0600

    Add content_for tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+ def page_title
+   if content_for?(:title)
+    content_for(:title)
+    else
+     "MyFoodPlanner"
+   end
+ end
+
   def bootstrap_flash_class(type)
     case type
     when 'alert' then 'warning'
