b981fb08d9c9a9b0862459b11ad7a7dec3d99eea
Anne Richardson <richardson.ae@gmail.com>
2019-02-28 18:50:08 -0600

    Add rails active libraries

--- spec/support/shoulda_matchers.rb
+++ spec/support/shoulda_matchers.rb
@@ -6,5 +6,8 @@ Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
+    
+    with.library :active_record
+    with.library :active_model
   end
 end
