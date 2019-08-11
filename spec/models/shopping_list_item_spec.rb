8d7318f62c05c2ef90bbd3fa48c3bd0f20a51b73
Anne Richardson <richardson.ae@gmail.com>
2019-08-11 18:48:21 -0500

    Add missing model specs

--- spec/models/shopping_list_item_spec.rb
+++ spec/models/shopping_list_item_spec.rb
@@ -15,4 +15,36 @@ RSpec.describe ShoppingListItem, type: :model do
       it { should validate_presence_of(:shopping_list_id) }
     end
   end
+
+  describe 'self.by_recently_edited' do
+    it 'returns the most recently editied item first' do
+      item1 = create(:shopping_list_item)
+      item2 = create(:shopping_list_item)
+      item3 = create(:shopping_list_item)
+      item2.purchased = true
+      item2.save
+
+      ordered_items = ShoppingListItem.by_recently_edited
+
+      expect(ordered_items.first).to eq(item2)
+    end
+  end
+
+  describe '#complete!' do
+    it 'sets the "purchased" attribute to true and saves the item' do
+      item = create(:shopping_list_item, purchased: false)
+      item.complete!
+
+      expect(item.purchased).to eq(true)
+    end
+  end
+
+  describe '#uncomplete!' do
+    it 'sets the "purchased" attribute to false and saves the item' do
+      item = create(:shopping_list_item, purchased: true)
+      item.uncomplete!
+
+      expect(item.purchased).to eq(false)
+    end
+  end
 end
