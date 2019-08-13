f07efc593ca96b35284cb85c6b22066460e0dabf
Anne Richardson <richardson.ae@gmail.com>
2019-08-13 07:14:29 -0500

    Only display qty if it is > 1 and move to end of line

--- app/helpers/shopping_lists_helper.rb
+++ app/helpers/shopping_lists_helper.rb
@@ -3,15 +3,19 @@
 module ShoppingListsHelper
   def toggle_favorite(list)
     if list.favorite
-      # Show the ★ and link to "unfavorite" it
+      # Show the filled star and link to "unfavorite" it
       link_to '', shopping_list_favorite_path(list),
                   class: Icon.star_filled,
                   method: :delete
     else
-      # Show the ☆ and link to "favorite" it
+      # Show the outlined star and link to "favorite" it
       link_to '', shopping_list_favorites_path(id: list.id),
                   class: Icon.star_outline,
                   method: :post
     end
+  end
+
+  def display_quantity(item)
+    "(#{item.qty_rounded})" if item.quantity > 1
   end
 end
