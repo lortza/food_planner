404e9833ab3bbbab19d09f11174f721dc05d1937
Anne Richardson <richardson.ae@gmail.com>
2019-08-18 16:58:52 -0500

    Catch up on specs

--- spec/models/bulk_list_item_manager_spec.rb
+++ spec/models/bulk_list_item_manager_spec.rb
@@ -21,31 +21,161 @@ RSpec.describe BulkListItemManager, type: :model do
     end
 
     context 'when an ingredient is new to the shopping list' do
-      xit 'the list item quantity equals the ingredient quantity' do
+      let(:shopping_list) { create(:shopping_list) }
+      let(:meal_plan) { create(:meal_plan) }
+      let(:recipe) { create(:recipe) }
+      let(:ingredient) { create(:ingredient, recipe: recipe, quantity: 1, measurement_unit: 'cup', name: 'rice') }
+
+      it 'the list item quantity equals the ingredient quantity' do
+        meal_plan.recipes << recipe
+        recipe.ingredients << ingredient
+
+        manager = BulkListItemManager.new(
+          shopping_list: shopping_list,
+          items_source: meal_plan
+        )
+        manager.add_items_to_list
+        item = shopping_list.items.last
+
+        expect(item.quantity).to eq(ingredient.quantity)
       end
 
-      xit 'is not crossed off' do
+      it 'is not crossed off' do
+        meal_plan.recipes << recipe
+        recipe.ingredients << ingredient
+
+        manager = BulkListItemManager.new(
+          shopping_list: shopping_list,
+          items_source: meal_plan
+        )
+        manager.add_items_to_list
+        item = shopping_list.items.last
+
+        expect(item.purchased).to eq(false)
       end
 
-      xit 'assigns the list item to the "Unassigned" aisle' do
+      it 'assigns the list item to the "Unassigned" aisle' do
+        meal_plan.recipes << recipe
+        recipe.ingredients << ingredient
+
+        manager = BulkListItemManager.new(
+          shopping_list: shopping_list,
+          items_source: meal_plan
+        )
+        manager.add_items_to_list
+        item = shopping_list.items.last
+
+        expect(item.aisle.name).to eq('unassigned')
       end
     end
 
-    context 'when an ingredient is already on the shopping list and has an aisle assigned' do
-      xit 'does not change that aisle assignment' do
-      end
-    end
+    context 'when an ingredient is already on the shopping list' do
+      let(:user) { create(:user) }
+      let(:meal_plan) { create(:meal_plan, user: user) }
+      let(:recipe) { create(:recipe, user: user) }
+      let(:ingredient) { create(:ingredient, recipe: recipe, quantity: 1, measurement_unit: 'cup', name: 'rice') }
+      let(:aisle) { create(:aisle, user: user, name: 'rice aisle') }
+      let(:shopping_list) { create(:shopping_list, user: user) }
+      let(:shopping_list_item) { create(:shopping_list_item, shopping_list: shopping_list, name: 'cup rice') }
 
-    context 'when an ingredient is already on the shopping list and not marked as crossed-off' do
-      xit "the incoming ingredient's quantity is added to the list item's quantity" do
-      end
-    end
+      context 'and has an aisle assigned' do
+        it 'does not change that aisle assignment' do
+          meal_plan.recipes << recipe
+          recipe.ingredients << ingredient
 
-    context 'when an ingredient is already on the shopping list and marked as crossed-off' do
-      xit "the list item's quantity matches the incoming ingredient.quantity" do
+          manager = BulkListItemManager.new(
+            shopping_list: shopping_list,
+            items_source: meal_plan
+          )
+          # add item for the first time
+          manager.add_items_to_list
+
+          item = shopping_list.items.last
+          expect(item.aisle.name).to eq('unassigned')
+
+          #assign an aisle to the item
+          item.update!(aisle: aisle)
+
+          # add the item for the second time
+          manager.add_items_to_list
+
+          expect(item.aisle.name).to eq('rice aisle')
+        end
       end
 
-      xit 'is no longer crossed off' do
+      context 'and not marked as crossed-off' do
+        it "the incoming ingredient's quantity is added to the list item's quantity" do
+          meal_plan.recipes << recipe
+          recipe.ingredients << ingredient
+
+          manager = BulkListItemManager.new(
+            shopping_list: shopping_list,
+            items_source: meal_plan
+          )
+          # add item for the first time
+          manager.add_items_to_list
+
+          item = shopping_list.items.last
+          expect(item.quantity).to eq(1)
+
+          # add the item for the second time
+          manager.add_items_to_list
+          item.reload
+
+          expect(item.quantity).to eq(2)
+        end
+      end
+
+      context 'and marked as crossed-off' do
+        it "the list item's quantity matches the incoming ingredient.quantity" do
+          meal_plan.recipes << recipe
+          recipe.ingredients << ingredient
+
+          manager = BulkListItemManager.new(
+            shopping_list: shopping_list,
+            items_source: meal_plan
+          )
+          # add item for the first time
+          manager.add_items_to_list
+
+          item = shopping_list.items.last
+          expect(item.quantity).to eq(1)
+
+          # cross off the item
+          item.update!(purchased: true)
+          item.reload
+
+          # add the item for the second time
+          manager.add_items_to_list
+          item.reload
+
+          expect(item.quantity).to eq(ingredient.quantity)
+        end
+
+        it 'is no longer crossed off' do
+          meal_plan.recipes << recipe
+          recipe.ingredients << ingredient
+
+          manager = BulkListItemManager.new(
+            shopping_list: shopping_list,
+            items_source: meal_plan
+          )
+          # add item for the first time
+          manager.add_items_to_list
+
+          item = shopping_list.items.last
+          expect(item.quantity).to eq(1)
+
+          # cross off the item
+          item.update!(purchased: true)
+          item.reload
+
+          # add the item for the second time
+          manager.add_items_to_list
+          item.reload
+
+          expect(item.purchased).to eq(false)
+        end
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,10 +13,17 @@ RSpec.describe User, type: :model do
   end
 
   describe '#favorite_list' do
-    xit 'returns a single shopping list' do
+    let(:user) { create(:user) }
+    let!(:fave_list) { create(:shopping_list, user: user, favorite: true) }
+    let!(:other_list) { create(:shopping_list, user: user, favorite: false) }
+
+    it 'returns a single shopping list' do
+      expect(user.favorite_list.class).to eq(ShoppingList)
     end
 
-    xit 'returns a shopping_list that is marked as favorite for this user' do
+    it 'returns a shopping_list that is marked as favorite for this user' do
+      expect(user.favorite_list).to eq(fave_list)
+      expect(user.favorite_list).not_to eq(other_list)
     end
   end
 end
