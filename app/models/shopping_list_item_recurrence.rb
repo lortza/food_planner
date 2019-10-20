28b409f61a4362c7d13d43f64b05aa958b58f678
Anne Richardson <richardson.ae@gmail.com>
2019-10-20 14:47:37 -0500

    Scout rubocopping

--- app/models/shopping_list_item_recurrence.rb
+++ app/models/shopping_list_item_recurrence.rb
@@ -1,40 +1,39 @@
 # frozen_string_literal: true
 
 class ShoppingListItemRecurrence
-  FREQUENCIES = ['weekly', 'monthly']
+  FREQUENCIES = %w[weekly monthly].freeze
 
   class << self
     def check_schedule
-      puts 'Checking schedule...'
-      today_is_weekly_item_day? ? add_items_to_list('weekly') : puts('No weekly items added. Today is not Tuesday.')
-      today_is_monthly_item_day? ? add_items_to_list('monthly') : puts('No monthly items added. Today is not last of month.')
+      Rails.logger.info('Checking schedule...')
+      today_is_weekly_item_day? ? add_items_to_list('weekly') : Rails.logger.info('No weekly items added.')
+      today_is_monthly_item_day? ? add_items_to_list('monthly') : Rails.logger.info('No monthly items added.')
     end
 
     private
 
     def add_items_to_list(frequency)
-      puts "Adding #{frequency} items to the list..."
+      Rails.logger.info("Adding #{frequency} items to the list.")
       items = ShoppingListItem.where(recurrence_frequency: frequency)
 
       items.each do |item|
-        puts item.name
+        Rails.logger.info(item.name)
         if item.purchased == true
           item.purchased = false
           item.quantity = item.recurrence_quantity
-          item.save
         else
           item.quantity += item.recurrence_quantity
-          item.save
         end
+        item.save
       end
     end
 
     def today_is_weekly_item_day?
-      Date.today.tuesday?
+      Time.zone.today.tuesday?
     end
 
     def today_is_monthly_item_day?
-      Date.today == Date.today.end_of_month
+      Time.zone.today == Time.zone.today.end_of_month
     end
   end
 end
