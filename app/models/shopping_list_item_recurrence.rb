4e3229849704ab0572fc1aee263e79dd1a9989aa
Anne Richardson <richardson.ae@gmail.com>
2019-11-01 10:57:30 -0500

    Add biweekly

--- app/models/shopping_list_item_recurrence.rb
+++ app/models/shopping_list_item_recurrence.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class ShoppingListItemRecurrence
-  FREQUENCIES = %w[weekly monthly].freeze
+  FREQUENCIES = %w[weekly biweekly monthly].freeze
 
   class << self
     def check_schedule
       Rails.logger.info('Checking schedule...')
       today_is_weekly_item_day? ? add_items_to_list('weekly') : Rails.logger.info('No weekly items added.')
+      today_is_biweekly_item_day? ? add_items_to_list('biweekly') : Rails.logger.info('No biweekly items added.')
       today_is_monthly_item_day? ? add_items_to_list('monthly') : Rails.logger.info('No monthly items added.')
     end
 
@@ -32,8 +33,19 @@ class ShoppingListItemRecurrence
       Time.zone.today.tuesday?
     end
 
+    def today_is_biweekly_item_day?
+      today = Date.current.day
+      second_week = (8..14).to_a
+      fourth_week = (22..31).to_a
+
+      (second_week + fourth_week).include?(today)
+    end
+
     def today_is_monthly_item_day?
-      Time.zone.today == Time.zone.today.end_of_month
+      the_month_this_week = Date.current.month
+      the_month_next_week = 1.week.from_now.month
+
+      the_month_this_week != the_month_next_week
     end
   end
 end
