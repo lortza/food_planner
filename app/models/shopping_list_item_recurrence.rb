f0f2a0274efce7de44374e89647d747940c96d2e
Anne Richardson <richardson.ae@gmail.com>
2019-11-04 07:26:18 -0600

    Ensure biweekly and monthly tasks only run on tuesdays

--- app/models/shopping_list_item_recurrence.rb
+++ app/models/shopping_list_item_recurrence.rb
@@ -6,9 +6,9 @@ class ShoppingListItemRecurrence
   class << self
     def check_schedule
       Rails.logger.info('Checking schedule...')
-      today_is_weekly_item_day? ? add_items_to_list('weekly') : Rails.logger.info('No weekly items added.')
-      today_is_biweekly_item_day? ? add_items_to_list('biweekly') : Rails.logger.info('No biweekly items added.')
-      today_is_monthly_item_day? ? add_items_to_list('monthly') : Rails.logger.info('No monthly items added.')
+      add_weekly_item? ? add_items_to_list('weekly') : Rails.logger.info('No weekly items added.')
+      add_biweekly_item? ? add_items_to_list('biweekly') : Rails.logger.info('No biweekly items added.')
+      add_monthly_item? ? add_items_to_list('monthly') : Rails.logger.info('No monthly items added.')
     end
 
     private
@@ -29,11 +29,23 @@ class ShoppingListItemRecurrence
       end
     end
 
+    def add_weekly_item?
+      today_is_weekly_item_day?
+    end
+
+    def add_biweekly_item?
+      today_is_weekly_item_day? && today_is_biweekly_week?
+    end
+
+    def add_monthly_item?
+      today_is_weekly_item_day? && today_is_last_week_of_month?
+    end
+
     def today_is_weekly_item_day?
       Time.zone.today.tuesday?
     end
 
-    def today_is_biweekly_item_day?
+    def today_is_biweekly_week?
       today = Date.current.day
       second_week = (8..14).to_a
       fourth_week = (22..31).to_a
@@ -41,7 +53,7 @@ class ShoppingListItemRecurrence
       (second_week + fourth_week).include?(today)
     end
 
-    def today_is_monthly_item_day?
+    def today_is_last_week_of_month?
       the_month_this_week = Date.current.month
       the_month_next_week = 1.week.from_now.month
 
