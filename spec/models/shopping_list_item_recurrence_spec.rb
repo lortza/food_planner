e3694778a9485a17e80b7edf5a721466ad46c77d
Anne Richardson <richardson.ae@gmail.com>
2019-11-04 08:52:23 -0600

    Update specs

--- spec/models/shopping_list_item_recurrence_spec.rb
+++ spec/models/shopping_list_item_recurrence_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe ShoppingListItemRecurrence, type: :model do
     end
 
     it 'adds recurring items to list on its scheduled day' do
-      allow(ShoppingListItemRecurrence).to receive(:today_is_weekly_item_day?).and_return(true)
+      allow(ShoppingListItemRecurrence).to receive(:add_weekly_item?).and_return(true)
       weekly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'weekly')
 
-      allow(ShoppingListItemRecurrence).to receive(:today_is_biweekly_item_day?).and_return(true)
+      allow(ShoppingListItemRecurrence).to receive(:add_biweekly_item?).and_return(true)
       biweekly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'biweekly')
 
-      allow(ShoppingListItemRecurrence).to receive(:today_is_monthly_item_day?).and_return(true)
+      allow(ShoppingListItemRecurrence).to receive(:add_monthly_item?).and_return(true)
       monthly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'monthly')
 
       ShoppingListItemRecurrence.check_schedule
@@ -28,13 +28,13 @@ RSpec.describe ShoppingListItemRecurrence, type: :model do
     end
 
     it 'does not add recurring items to list on on non-scheduled days' do
-      allow(ShoppingListItemRecurrence).to receive(:today_is_weekly_item_day?).and_return(false)
+      allow(ShoppingListItemRecurrence).to receive(:add_weekly_item?).and_return(false)
       weekly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'weekly')
 
-      allow(ShoppingListItemRecurrence).to receive(:today_is_biweekly_item_day?).and_return(false)
+      allow(ShoppingListItemRecurrence).to receive(:add_biweekly_item?).and_return(false)
       biweekly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'biweekly')
 
-      allow(ShoppingListItemRecurrence).to receive(:today_is_monthly_item_day?).and_return(false)
+      allow(ShoppingListItemRecurrence).to receive(:add_monthly_item?).and_return(false)
       monthly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'monthly')
 
       ShoppingListItemRecurrence.check_schedule
@@ -45,7 +45,7 @@ RSpec.describe ShoppingListItemRecurrence, type: :model do
     end
 
     it 'moves the item to "purchased" if the item is unpurchased' do
-      allow(ShoppingListItemRecurrence).to receive(:today_is_weekly_item_day?).and_return(true)
+      allow(ShoppingListItemRecurrence).to receive(:add_weekly_item?).and_return(true)
       weekly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'weekly')
       ShoppingListItemRecurrence.check_schedule
 
@@ -53,7 +53,7 @@ RSpec.describe ShoppingListItemRecurrence, type: :model do
     end
 
     it 'inserts the recurrence qty as the qty if the item had been purchased' do
-      allow(ShoppingListItemRecurrence).to receive(:today_is_weekly_item_day?).and_return(true)
+      allow(ShoppingListItemRecurrence).to receive(:add_weekly_item?).and_return(true)
       weekly_item = create(:shopping_list_item, purchased: true, recurrence_frequency: 'weekly', quantity: 2, recurrence_quantity: 1)
       ShoppingListItemRecurrence.check_schedule
 
@@ -61,7 +61,7 @@ RSpec.describe ShoppingListItemRecurrence, type: :model do
     end
 
     it 'adds the recurrence qty to the existing qty if is is not purchased' do
-      allow(ShoppingListItemRecurrence).to receive(:today_is_weekly_item_day?).and_return(true)
+      allow(ShoppingListItemRecurrence).to receive(:add_weekly_item?).and_return(true)
       weekly_item = create(:shopping_list_item, purchased: false, recurrence_frequency: 'weekly', quantity: 2, recurrence_quantity: 1)
       ShoppingListItemRecurrence.check_schedule
 
