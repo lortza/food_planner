2efc3dd45e071cdea9c5c45b20c6eeafb32509ff
Anne Richardson <richardson.ae@gmail.com>
2019-11-16 18:05:06 -0600

    WIP toggle js

--- app/assets/javascripts/shopping_lists.js
+++ app/assets/javascripts/shopping_lists.js
@@ -3,8 +3,31 @@ function shoppingListItemToggler() {
     let activeSection = document.getElementById('active-items')
     let inactiveSection = document.getElementById('inactive-items')
 
-    console.log(activeSection);
-    console.log('holla')
+    activeSection.addEventListener('click', function(e){
+      let item = e.target;
+      let itemHtml = item.parentNode.parentNode;
+
+      if ( item.classList.contains('js-toggle') ) {
+        itemHtml.remove();
+
+        console.log(itemHtml)
+        let crossedOffItem = itemHtml.classList.add('item-crossed-off').outerHTML
+        console.log(crossedOffItem)
+        inactiveSection.insertAdjacentHTML('afterbegin', itemHtml.outerHTML)
+      }
+    })
+
+    // inactiveSection.addEventListener('click', function(e){
+    //   let item = e.target;
+    //   let itemHtml = item.parentNode.parentNode;
+    //
+    //   if ( item.classList.contains('js-toggle') ) {
+    //     itemHtml.remove();
+    //
+    //     let reactivatedItem = itemHtml.classList.remove('item-crossed-off').outerHTML
+    //     inactiveSection.insertAdjacentHTML('afterbegin', reactivatedItem)
+    //   }
+    // })
 
   });
 };
