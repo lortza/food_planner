f1ef4ff562d66ffa86ddaf5c8a9e388bcb7d54ad
Anne Richardson <richardson.ae@gmail.com>
2019-11-23 09:11:12 -0600

    Add recipe_id to params handling

--- app/controllers/shopping_list_item_builders_controller.rb
+++ app/controllers/shopping_list_item_builders_controller.rb
@@ -2,15 +2,21 @@
 
 class ShoppingListItemBuildersController < ApplicationController
   def create
+    recipe = Recipe.find(permitted_params[:recipe_id]) if permitted_params[:recipe_id]
+    meal_plan = MealPlan.find(permitted_params[:meal_plan_id]) if permitted_params[:meal_plan_id]
+
     builder = ShoppingListItemBuilder.new(
       shopping_list: ShoppingList.find(permitted_params[:shopping_list_id]),
       single_ingredient: Ingredient.where(id: permitted_params[:ingredient_id]),
-      meal_plan: MealPlan.find(permitted_params[:meal_plan_id]),
+      meal_plan: meal_plan,
+      recipe: recipe
     )
     builder.add_items_to_list
 
     flash[:success] = "#{ActionController::Base.helpers.pluralize(builder.ingredients.count, 'item')} added."
-    redirect_to builder.meal_plan
+
+    redirect_path = meal_plan.present? ? meal_plan : recipe
+    redirect_to redirect_path
   end
 
   private
@@ -18,6 +24,7 @@ class ShoppingListItemBuildersController < ApplicationController
   def permitted_params
     params.permit(:shopping_list_id,
                   :meal_plan_id,
+                  :recipe_id,
                   :ingredient_id)
   end
 end
