9813154f6bfa1d282b03d17643dc69283107c2af
mike vallano <mikevallano@gmail.com>
2019-11-24 09:54:36 -0600

    Refactor shopping list item builder to need only shopping list id and ingredient ids

--- app/controllers/shopping_list_item_builders_controller.rb
+++ app/controllers/shopping_list_item_builders_controller.rb
@@ -2,29 +2,20 @@
 
 class ShoppingListItemBuildersController < ApplicationController
   def create
-    recipe = Recipe.find(permitted_params[:recipe_id]) if permitted_params[:recipe_id]
-    meal_plan = MealPlan.find(permitted_params[:meal_plan_id]) if permitted_params[:meal_plan_id]
-
     builder = ShoppingListItemBuilder.new(
-      shopping_list: ShoppingList.find(permitted_params[:shopping_list_id]),
-      single_ingredient: Ingredient.where(id: permitted_params[:ingredient_id]),
-      meal_plan: meal_plan,
-      recipe: recipe
+      shopping_list_id: permitted_params[:shopping_list_id],
+      ingredient_ids: permitted_params[:ingredient_ids]
     )
     builder.add_items_to_list
 
     flash[:success] = "#{ActionController::Base.helpers.pluralize(builder.ingredients.count, 'item')} added."
 
-    redirect_path = meal_plan.present? ? meal_plan : recipe
-    redirect_to redirect_path
+    redirect_back(fallback_location: root_path)
   end
 
   private
 
   def permitted_params
-    params.permit(:shopping_list_id,
-                  :meal_plan_id,
-                  :recipe_id,
-                  :ingredient_id)
+    params.permit(:shopping_list_id, ingredient_ids: [])
   end
 end
