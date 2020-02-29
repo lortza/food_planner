07a7f6c21fe29855fcae831f3cff6bb452161234
Anne Richardson <richardson.ae@gmail.com>
2020-02-29 11:49:38 -0600

    Got it working the JankyTM way

--- app/controllers/shopping_list_item_builders_controller.rb
+++ app/controllers/shopping_list_item_builders_controller.rb
@@ -4,7 +4,8 @@ class ShoppingListItemBuildersController < ApplicationController
   def create
     builder = ShoppingListItemBuilder.new(
       shopping_list_id: permitted_params[:shopping_list_id],
-      ingredient_ids: permitted_params[:ingredient_ids]
+      # ingredient_ids: permitted_params[:ingredient_ids]
+      ingredient_ids: permitted_params[:ingredientids].split(' ').map(&:to_i)
     )
     builder.add_items_to_list
 
@@ -16,6 +17,6 @@ class ShoppingListItemBuildersController < ApplicationController
   private
 
   def permitted_params
-    params.permit(:shopping_list_id, ingredient_ids: [])
+    params.permit(:shopping_list_id, :ingredientids, ingredient_ids: [])
   end
 end
--- spec/helpers/recipes_helper_spec.rb
+++ spec/helpers/recipes_helper_spec.rb
@@ -38,4 +38,24 @@ RSpec.describe RecipesHelper, type: :helper do
       expect(helper.status_flag(recipe)).to eq('')
     end
   end
+
+  describe 'recipe_ingredient_ids' do
+    it 'returns an array of ingredient ids from this recipe' do
+      recipe = create(:recipe, :with_2_ingredients)
+
+      expect(helper.recipe_ingredient_ids(recipe)).to be_a(Array)
+      expect(helper.recipe_ingredient_ids(recipe).length).to eq(2)
+    end
+
+    it "returns only ids for this recipe's ingredients" do
+      recipe1 = create(:recipe, :with_2_ingredients)
+      recipe2 = create(:recipe, :with_2_ingredients)
+
+      recipe1_results = recipe1.ingredients.pluck(:id)
+      expect(helper.recipe_ingredient_ids(recipe1)).to eq(recipe1_results)
+
+      recipe2_results = recipe2.ingredients.pluck(:id)
+      expect(helper.recipe_ingredient_ids(recipe1)).to_not eq(recipe2_results)
+    end
+  end
 end
