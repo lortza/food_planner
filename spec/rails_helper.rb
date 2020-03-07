4db6a197d0ff9e72035a43fc16763992d073ae37
Anne Richardson <richardson.ae@gmail.com>
2020-03-07 16:02:50 -0600

    Add capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ require 'rspec/rails'
 require 'pry'
 require 'support/factory_bot'
 require 'support/shoulda_matchers'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
