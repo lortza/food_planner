fe36048431f572011fbb6de15f8be4384dc38255
Anne Richardson <richardson.ae@gmail.com>
2020-03-28 16:14:41 -0500

    Feature spec for experimental recipe conversion

--- spec/features/convert_experimental_to_recipe.rb
+++ spec/features/convert_experimental_to_recipe.rb
@@ -1,48 +1,47 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "the signin process", type: :feature do
-  before :each do
-    User.create(email: 'user@example.com', password: 'password')
-  end
+describe 'User converts an experimental recipe to a recipe', type: :feature do
+  let(:user) { create(:user) }
+  let(:experimental_recipe) { create(:experimental_recipe, user: user) }
 
-  it "signs me in" do
+  before do
+    experimental_recipe
+
     visit '/users/sign_in'
-    within("#new_user") do
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Password', with: 'password'
+    within('#new_user') do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
     end
     click_button 'Log in'
-    expect(page).to have_content 'Signed in'
+
+    visit(experimental_recipes_path)
+    click_link('convert to recipe')
+  end
+
+  it 'populates a new recipe with existing data' do
+    expect(page).to have_content('Create a Recipe')
+    expect(page).to have_field('Title', with: experimental_recipe.title)
+    expect(page).to have_field('Source url', with: experimental_recipe.source_url)
+  end
+
+  it 'Experimenal recipe is deleted after new recipe is saved' do
+    fill_in 'Servings', with: '4'
+    fill_in 'recipe_prep_time', with: '20'
+    fill_in 'recipe_cook_time', with: '20'
+    fill_in 'recipe_image_url', with: 'recipe_placeholder.jpg'
+    find('#recipe_ingredients_attributes_0_quantity').set('1')
+    find('#recipe_ingredients_attributes_0_quantity').set('1')
+    select 'cup', from: 'recipe_ingredients_attributes_0_measurement_unit'
+    find('#recipe_ingredients_attributes_0_name').set('water')
+    find('#recipe_instructions').set('Lorem ipsum')
+
+    click_button('Create Recipe')
+    expect(page).to have_content(experimental_recipe.title)
+
+    visit(experimental_recipes_path)
+    expect(page).to_not have_content(experimental_recipe.title)
+
+    expect(user.recipes.count).to eq(1)
+    expect(user.experimental_recipes.count).to eq(0)
   end
 end
-
-# xfeature 'User converts an experimental recipe to a recipe' do
-#   let(:experimental_recipe) { create(:experimental_recipe) }
-#   let(:user) { create(:user) }
-#
-#   before do
-#     sign_in(user)
-#     visit(experimental_recipes_path)
-#   end
-#
-#   it 'populates a new recipe with existing data' do
-#     click_link('convert to recipe')
-#
-#     expect(page).to have_content("Create a Recipe")
-#     expect(page).to have_content(experimental_recipe.title)
-#     expect(page).to have_content(experimental_recipe.url)
-#   end
-#
-#   it 'Experimenal recipe is deleted after new recipe is saved' do
-#     click_link('convert to recipe')
-#
-#     # fill out servings
-#     # fill out prep time
-#     # fill out 1 ingredient
-#     # fill out instructions
-#     # click submit
-#
-#     experimental_recipe.reload
-#     expect(experimental_recipe).to eq(nil)
-#   end
-# end
