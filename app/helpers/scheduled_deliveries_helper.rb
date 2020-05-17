1814d838eccc8c944035b1515aa6fcd6fb44d334
Anne Richardson <richardson.ae@gmail.com>
2020-05-17 15:07:21 -0500

    Add CRUD functions for deliveries

--- app/helpers/scheduled_deliveries_helper.rb
+++ app/helpers/scheduled_deliveries_helper.rb
@@ -3,7 +3,7 @@
 module ScheduledDeliveriesHelper
   def display_scheduled_deliveries(deliveries)
     deliveries.map do |delivery|
-      "#{delivery.service_provider} #{delivery.scheduled_for.to_s(:timestamp)}".squish
-    end.join(' | ')
+      link_to ("#{delivery.service_provider} #{delivery.scheduled_for.to_s(:timestamp)}".squish), edit_shopping_list_scheduled_delivery_path(delivery.shopping_list, delivery)
+    end.join(' | ').html_safe
   end
 end
