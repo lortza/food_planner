dcdf6ecd7bf5b78fe6fa0fe09b02e12c23368c25
Anne Richardson <richardson.ae@gmail.com>
2020-09-14 19:03:01 -0500

    Add link to flash notice

--- app/helpers/styles_helper.rb
+++ app/helpers/styles_helper.rb
@@ -4,4 +4,18 @@ module StylesHelper
   def button_classes(style = 'primary')
     "btn btn-sm btn-#{style}"
   end
+
+  def bootstrap_flash_class(type)
+    modifier = case type
+    when 'alert' then 'warning'
+    when 'error' then 'danger'
+    when 'warning' then 'warning'
+    when 'notice' then 'info'
+    when 'success' then 'success'
+    else
+      'info'
+    end
+
+    "alert alert-#{modifier} fadein mt-3"
+  end
 end
