4b593c627bb6e62f8a3fdd5d6165f9496cf2ec61
Anne Richardson <richardson.ae@gmail.com>
2020-12-13 16:45:58 -0600

    Upgrade to rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,13 +11,15 @@ module FoodPlanner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
 
+    # config.time_zone = "Central Time (US & Canada)"
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # config.eager_load_paths << Rails.root.join("extras")
 
     Raven.configure do |config|
       config.dsn = Rails.application.credentials.sentry_raven_dsn
