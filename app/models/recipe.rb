d17040575eef6c75dece15e36007ca321939fe3a
Anne Richardson <richardson.ae@gmail.com>
2021-11-17 07:56:38 -0600

    WIP heredoc version

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -58,7 +58,55 @@ class Recipe < ApplicationRecord
       )
       .distinct
   end
+
+
+  # Where I left off...
+  # I'm trying to come up with an example of how to do this heredoc version of a sql query
+  # but i am having trouble with the :v_search_terms being read like '%':v_search_terms'%'
+  # instead of '%:v_search_terms%' 
+  def self.cool_search_w_sql(search_terms = '')
+    stripped_terms = search_terms&.gsub(',', '')&.squish
+
+
+    # Example of conditional SQL: Use ruby to make a line of SQL that's conditional and set it to a variable. We'll interpolate it below.
+    # sql_bin = 'AND bp.bin = :v_bin' if bin.present?
+    # sql_pcn = 'AND bp.pcn = :v_pcn' if pcn.present?
+
+    query = <<~SQL
+      SELECT DISTINCT r.title, r.source_url, r.source_name
+      FROM recipes r
+      LEFT JOIN ingredients i on i.recipe_id = r.id
+      WHERE concat_ws(' ', r.title, r.source_name, r.extra_work_note) ILIKE '%:v_search_terms%'
+      OR i.name ILIKE '%:v_search_terms%'
+      ORDER BY r.title
+      ;
+    SQL
+
+    # SELECT * FROM puf_plan_information pi
+    #   -- This `:v_search_terms` syntax with the colon is coming from the sanitized params.
+    #   -- I'm using the `v_` to help disambiguate these sanitized params from the args coming in the
+    #   -- top of the method.
+    #   WHERE pi.file_name = :v_file_name
+    #    AND pi.state = :v_state
+    #    AND pi.premium IS NOT NULL
+    #    AND pi.plan_id IS NOT NULL
+    #   -- If you have simple sql and no outside value being passed in, it is safe to
+    #   -- straight interpolate the sql into this string like so:
+    #    #{sql_bin}
+    #    #{sql_pcn}
+    #   -- In our case, we needed to use a sanitized version (up at the top) of the sql before interpolating it.
+    #  ORDER BY pi.plan_name
+
+    # We're about to sanitize these params below...
+    query_params = {
+      v_search_terms: stripped_terms
+    }
+    ActiveRecord::Base.connection.execute(
+      # ...and this is where they get sanitized.
+      ActiveRecord::Base.sanitize_sql_for_conditions([query, query_params]),
+    ).to_a
   end
+
 
   def self.by_last_prepared
     order('meal_plans.prepared_on asc')
