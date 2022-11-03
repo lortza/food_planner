5fc8facb8a6062758bf6d471d5f088272f74a1f0
Anne Richardson <richardson.ae@gmail.com>
2022-11-03 08:48:35 -0500

    Ensure UNIT items are in order

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,16 +4,7 @@ class Ingredient < ApplicationRecord
   belongs_to :recipe, inverse_of: :ingredients
   before_save :format_name
 
-  # Units that do not get pluralized
-  DESCRIPTIVE_UNITS = %w[
-    small
-    medium
-    large
-    whole
-  ].freeze
-
-  # Standard units
-  UNITS = %w[
+  STANDARD_UNITS = %w[
     4oz\ can
     7oz\ can
     15oz\ can
@@ -40,7 +31,20 @@ class Ingredient < ApplicationRecord
     stick
     tablespoon
     teaspoon
-  ] + DESCRIPTIVE_UNITS
+  ].freeze
+
+  # Units that do not get pluralized
+  DESCRIPTIVE_UNITS = %w[
+    small
+    medium
+    large
+    whole
+  ].freeze
+
+  UNITS = [
+    STANDARD_UNITS,
+    DESCRIPTIVE_UNITS,
+  ].flatten.sort
 
   STYLES = %w[
     \
@@ -71,22 +75,6 @@ class Ingredient < ApplicationRecord
     uncooked
   ].freeze
 
-  # KNOWN_FOODS = %w[
-  #   agave\ nectar
-  #   apple\ cider\ vinegar
-  #   coconut\ oil
-  #   fresh\ ginger
-  #   garlic
-  #   green\ bell\ pepper
-  #   green\ onions
-  #   honey
-  #   lime
-  #   olive\ oil
-  #   onion
-  #   red\ bell\ pepper
-  #   toasted\ sesame \oil
-  # ].freeze
-
   validates :name,
             :quantity,
             :measurement_unit,
@@ -94,7 +82,6 @@ class Ingredient < ApplicationRecord
 
   validates :quantity, numericality: true
   validates :measurement_unit, inclusion: { in: UNITS }
-  # validates :preparation_style, inclusion: { in: STYLES }, if: -> { preparation_style.present? }
 
   def self.by_id
     order(:id)
@@ -107,31 +94,4 @@ class Ingredient < ApplicationRecord
   def measurement_and_name
     "#{measurement_unit} #{name}"
   end
-
-  # def to_shopping_list_item
-  #   ShoppingListItem.new(
-  #     quantity: self.quantity,
-  #     name: "#{self.measurement_unit} #{self.name}"
-  #   )
-  # end
-
-  # def convert_to_food
-  #   'WIP'
-  # end
-
-  # def self.parse(list)
-  #   %Q(
-  #     1/2 cup olive oil
-  #     1 red bell pepper, seeded and chopped into big pieces
-  #     2 tablespoons apple cider vinegar
-  #     1 lime, juiced
-  #     2 tablespoons agave nectar
-  #     3/4 inch piece of fresh ginger, peeled and roughly chopped
-  #     1 clove garlic
-  #     totally optional: 1/4 teaspoon toasted sesame oil
-  #   ).split("\n").map(&:strip)
-  # end
-
-  # def to_float
-  # end
 end
