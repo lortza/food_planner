cadca13f7b000ef41f0465ce4fd9cd86ee3c6e11
Anne Richardson <richardson.ae@gmail.com>
2023-04-07 11:43:55 -0400

    Update specs for 7.0.4.3

--- app/helpers/scheduled_deliveries_helper.rb
+++ app/helpers/scheduled_deliveries_helper.rb
@@ -4,7 +4,7 @@ module ScheduledDeliveriesHelper
   # rubocop disable: Rails/OutputSafety
   def display_scheduled_deliveries(deliveries)
     deliveries.map do |delivery|
-      link_to "#{delivery.service_provider} #{delivery.scheduled_for.to_s(:timestamp)}".squish,
+      link_to "#{delivery.service_provider} #{delivery.scheduled_for.to_fs(:timestamp)}".squish,
               edit_shopping_list_scheduled_delivery_path(delivery.shopping_list, delivery)
     end.join(' | ').html_safe
   end
