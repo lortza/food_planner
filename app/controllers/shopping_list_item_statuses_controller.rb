52e3f9fa8336ac3909623a7ce5de12096b2f4027
richardsonae <richardson.ae@gmail.com>
2023-10-18 15:40:43 -0400

    Remove aisle when empty (#764)

--- app/controllers/shopping_list_item_statuses_controller.rb
+++ app/controllers/shopping_list_item_statuses_controller.rb
@@ -21,6 +21,7 @@ class ShoppingListItemStatusesController < ApplicationController
   def deactivate
     # crosses an item off of the list
     @shopping_list_item.deactivate!
+    @remove_aisle = @shopping_list_item.list.items.active.where(aisle: @shopping_list_item.aisle).blank?
     respond_to :js
   end
 
