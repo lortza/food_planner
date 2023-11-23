a50f36e4128a2bc59b66943bb750f06dba838301
Anne Richardson <richardson.ae@gmail.com>
2023-11-23 10:43:53 -0500

    Add support for filled icons

--- app/components/material_icon.rb
+++ app/components/material_icon.rb
@@ -4,11 +4,12 @@ class MaterialIcon
   # https://fonts.google.com/icons
   include ActionView::Helpers::TagHelper
 
-  def initialize(icon:, title: nil, size: :inherit, classes: nil)
+  def initialize(icon:, title: nil, size: :inherit, filled: false, classes: nil)
     @icon = icon
     @title = title
     @size = size
-    @classes = classes
+    @filled = filled ? 'icon-filled' : 'icon-outlined'
+    @provided_classes = classes
   end
 
   def render
@@ -30,8 +31,7 @@ class MaterialIcon
     when :settings then settings;
     when :shopping_bag then shopping_bag;
     when :shopping_cart then shopping_cart;
-    when :star_filled then star_filled;
-    when :star_outline then star_outline;
+    when :star then star;
     when :sync then sync;
     when :trash then trash;
     when :truck then truck;
@@ -44,134 +44,128 @@ class MaterialIcon
 
   def add_shopping_cart
     content_tag(:span, 'add_shopping_cart',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Add to cart')
   end
 
   def arrow_left
     content_tag(:span, 'arrow_back',
-      class: "#{symbol_base_classes} #{@classes} nav-link-icon left",
+      class: "#{symbol_classes} nav-link-icon left",
       title: @title.presence || 'Back')
   end
 
   def archived
     content_tag(:span, 'inventory_2',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Archived')
   end
 
   def calendar_clock
     content_tag(:span, 'calendar_clock',
-      class: "#{symbol_base_classes} #{@classes} text-warning",
+      class: "#{symbol_classes} text-warning",
       title: @title.presence || "It's been a while")
   end
 
   def clock
     content_tag(:span, 'update',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Duration warning')
   end
 
   def checkmark
     content_tag(:span, 'done',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Done')
   end
 
   def copy
     content_tag(:span, 'file_copy',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Copy')
   end
 
   def edit_note
     content_tag(:span, 'edit_note',
-      class: "#{symbol_base_classes} #{@classes} float-right",
+      class: "#{symbol_classes} float-right",
       title: @title.presence || 'Edit')
   end
 
   def event_repeat
     content_tag(:span, 'event_repeat',
-      class: "#{symbol_base_classes} #{@classes}",
-      title: @title.presence || 'Sync')
+      class: symbol_classes,
+      title: @title.presence || 'Repeated')
   end
 
   def info
     content_tag(:span, 'info',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Info')
   end
 
   def new_release
     content_tag(:span, 'new_releases',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'New')
   end
 
   def plus_circle
     content_tag(:span, 'add_circle',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Add')
   end
 
   def plus_square
     content_tag(:span, 'add_box',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Add to list')
   end
 
   def shopping_bag
     content_tag(:span, 'shopping_bag',
-      class: "#{icon_base_classes} #{@classes}",
+      class: icon_classes,
       title: @title.presence || 'In shopping bag')
   end
 
   def shopping_cart
     content_tag(:span, 'shopping_cart',
-      class: "#{icon_base_classes} #{@classes}",
+      class: icon_classes,
       title: @title.presence || 'Cart')
   end
 
-  def star_filled
+  def star
     content_tag(:span, 'star',
-      class: "#{icon_base_classes} #{@classes}",
-      title: @title.presence || 'Starred')
-  end
-
-  def star_outline
-    content_tag(:span, 'star',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Star')
   end
 
   def settings
     content_tag(:span, 'settings',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Settings')
   end
 
   def search
     content_tag(:span, 'search',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Search',
       aria: {hidden: true})
   end
 
   def sync
     content_tag(:span, 'sync',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Sync')
   end
 
   def trash
     content_tag(:span, 'delete',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Delete')
   end
 
   def truck
     content_tag(:span, 'local_shipping',
-      class: "#{symbol_base_classes} #{@classes}",
+      class: symbol_classes,
       title: @title.presence || 'Delivery')
   end
 
@@ -189,15 +183,20 @@ class MaterialIcon
     end
   end
 
-  def symbol_base_classes
-    "material-symbols-outlined #{base_classes}"
+  def symbol_classes
+    "material-symbols-outlined #{computed_classes}"
   end
 
-  def icon_base_classes
-    "material-icons-outlined #{base_classes}"
+  def icon_classes
+    # icon classes do not support "filled"
+    "material-icons-outlined #{computed_classes}"
+  end
+
+  def computed_classes
+    "#{base_classes} #{size_class} #{@filled} #{@provided_classes}"
   end
 
   def base_classes
-    "#{size_class} align-middle"
+    "align-middle"
   end
 end
