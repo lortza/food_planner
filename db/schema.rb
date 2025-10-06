0d1296c698e1d454f60f0990860d54e2544822e9
Anne Richardson <richardson.ae@gmail.com>
2025-10-06 15:00:55 -0400

    Data migration for converting experimental recipes to pending recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_02_174907) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_06_174506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
