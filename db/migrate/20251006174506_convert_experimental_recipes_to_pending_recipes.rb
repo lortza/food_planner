29c5ddd5497c0dfdfdfa931439b7998c2228a9a7
Anne Richardson <richardson.ae@gmail.com>
2025-10-07 11:39:09 -0400

    Comment out data migrations that have already run in prod

--- db/migrate/20251006174506_convert_experimental_recipes_to_pending_recipes.rb
+++ db/migrate/20251006174506_convert_experimental_recipes_to_pending_recipes.rb
@@ -1,29 +1,30 @@
-class ConvertExperimentalRecipesToPendingRecipes < ActiveRecord::Migration[7.2]
-  def up
-    ExperimentalRecipe.all.each do |experimental_recipe|
-      Recipe.create(
-        title: experimental_recipe.title,
-        source_name: URI.parse(experimental_recipe.source_url).host.gsub("www.", ""),
-        source_url: experimental_recipe.source_url,
-        image_url: experimental_recipe.image_url,
-        instructions: Scraper.new(experimental_recipe.source_url).site_data,
-        user_id: experimental_recipe.user.id,
-        created_at: experimental_recipe.created_at,
-        status: :pending
-      )
-      experimental_recipe.destroy
-    end
-  end
+# This data migration has already been run in production and is kept here for reference.
+# class ConvertExperimentalRecipesToPendingRecipes < ActiveRecord::Migration[7.2]
+#   def up
+#     ExperimentalRecipe.all.each do |experimental_recipe|
+#       Recipe.create(
+#         title: experimental_recipe.title,
+#         source_name: URI.parse(experimental_recipe.source_url).host.gsub("www.", ""),
+#         source_url: experimental_recipe.source_url,
+#         image_url: experimental_recipe.image_url,
+#         instructions: Scraper.new(experimental_recipe.source_url).site_data,
+#         user_id: experimental_recipe.user.id,
+#         created_at: experimental_recipe.created_at,
+#         status: :pending
+#       )
+#       experimental_recipe.destroy
+#     end
+#   end
 
-  def down
-    Recipe.where(status: :pending).each do |recipe|
-      ExperimentalRecipe.create(
-        title: recipe.title,
-        source_url: recipe.source_url,
-        image_url: recipe.image_url,
-        user_id: recipe.user.id
-      )
-      recipe.destroy
-    end
-  end
-end
+#   def down
+#     Recipe.where(status: :pending).each do |recipe|
+#       ExperimentalRecipe.create(
+#         title: recipe.title,
+#         source_url: recipe.source_url,
+#         image_url: recipe.image_url,
+#         user_id: recipe.user.id
+#       )
+#       recipe.destroy
+#     end
+#   end
+# end
