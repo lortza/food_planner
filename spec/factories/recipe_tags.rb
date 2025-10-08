cd1303324ad85ea6f133f162d03fb32b2d87f877
Anne Richardson <richardson.ae@gmail.com>
2025-10-08 16:44:22 -0400

    Annotate recipe_tags factory

--- spec/factories/recipe_tags.rb
+++ spec/factories/recipe_tags.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: recipe_tags
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  recipe_id  :bigint           not null
+#  tag_id     :bigint           not null
+#
+# Indexes
+#
+#  index_recipe_tags_on_recipe_id             (recipe_id)
+#  index_recipe_tags_on_recipe_id_and_tag_id  (recipe_id,tag_id) UNIQUE
+#  index_recipe_tags_on_tag_id                (tag_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (recipe_id => recipes.id)
+#  fk_rails_...  (tag_id => tags.id)
+#
 FactoryBot.define do
   factory :recipe_tag do
     recipe
