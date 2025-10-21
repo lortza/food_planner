b6f0e786e1b1acd5eae08020d22191e4e27c138b
Anne Richardson <richardson.ae@gmail.com>
2025-10-21 09:52:37 -0400

    Add controller actions and specs for rest of tag CRUD

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,53 +1,54 @@
 # frozen_string_literal: true
 
 class TagsController < ApplicationController
-  before_action :set_tag, only: %i[show]
+  before_action :set_tag, only: %i[show edit update destroy]
 
-  # def index
-  #   @tags = current_user.tags
-  # end
+  def index
+    @tags = current_user.tags
+  end
 
-  # def new
-  #   @tag = current_user.tags.new
-  # end
+  def new
+    @tag = current_user.tags.new
+    authorize(@tag)
+  end
 
-  # def create
-  #   @tag = current_user.tags.new(tag_params)
-  #   authorize(@tag)
+  def create
+    @tag = current_user.tags.new(tag_params)
+    authorize(@tag)
 
-  #   if @tag.save
-  #     redirect_to tags_url
-  #   else
-  #     render :new
-  #   end
-  # end
+    if @tag.save
+      redirect_to tags_url
+    else
+      render :new
+    end
+  end
 
   def show
     authorize(@tag)
   end
 
-  # def edit
-  #   # authorize(@tag)
-  # end
+  def edit
+    authorize(@tag)
+  end
 
-  # def update
-  #   # authorize(@tag)
+  def update
+    authorize(@tag)
 
-  #   if @tag.update(tag_params)
-  #     redirect_to tags_url
-  #   else
-  #     render :edit
-  #   end
-  # end
+    if @tag.update(tag_params)
+      redirect_to tags_url
+    else
+      render :edit
+    end
+  end
 
-  # def destroy
-  #   tag = Tag.find(params[:id])
-  #   authorize(tag)
+  def destroy
+    tag = Tag.find(params[:id])
+    authorize(tag)
 
-  #   tag.destroy
-  #   flash[:success] = "tag deleted"
-  #   redirect_to tags_path
-  # end
+    tag.destroy
+    flash[:success] = "tag deleted"
+    redirect_to tags_path
+  end
 
   private
 
--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -11,8 +11,53 @@ RSpec.describe "Tags", type: :request do
       user_tag
     end
 
+    it "denies access to tags#index" do
+      get tags_path(user_tag.id)
+
+      expect(response).to have_http_status(401)
+    end
+
+    it "denies access to tags#new" do
+      get new_tag_path
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "denies access to tags#create" do
+      tag_attributes = build(:tag, user: user).attributes
+
+      expect {
+        post tags_path(tag_attributes)
+      }.to_not change(Tag, :count)
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "denies access to tags#show" do
       get tag_path(user_tag.id)
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "denies access to tags#edit" do
+      get tag_path(user_tag.id)
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "denies access to tags#update" do
+      patch tag_path(user_tag, tag: user_tag.attributes)
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "denies access to tags#destroy" do
+      delete tag_path(user_tag)
 
       expect(response).to have_http_status(302)
       expect(response).to redirect_to new_user_session_path
@@ -24,32 +69,78 @@ RSpec.describe "Tags", type: :request do
     let(:user_tag) { create(:tag, user: user) }
 
     before :each do
-      sign_in user
       user_tag
+      sign_in(user)
     end
 
-    it "allows access to tags#show" do
-      get tag_path(user_tag.id)
+    it "renders tags#new" do
+      get new_tag_path
 
-      expect(response).to have_http_status(200)
+      expect(response).to be_successful
+      expect(response).to render_template(:new)
+    end
+
+    it "renders tags#edit" do
+      get edit_tag_path(user_tag.id)
+
+      expect(response).to be_successful
+      expect(response).to render_template(:edit)
+    end
+
+    it "renders tags#create" do
+      starting_count = Tag.count
+      tag_attributes = build(:tag, user: user).attributes
+      post tags_path(tag: tag_attributes)
+
+      expect(Tag.count).to eq(starting_count + 1)
+    end
+
+    it "renders tags#update" do
+      new_name = "different name"
+      patch tag_path(user_tag, tag: {name: new_name})
+
+      expect(response).to redirect_to tags_url
+    end
+
+    it "renders tags#destroy" do
+      delete tag_path(user_tag)
+
+      expect(response).to redirect_to(tags_url)
     end
   end
 
   describe "Authenticated access to another user's tags" do
-    let(:user) { create(:user) }
-    let(:different_user) { create(:user) }
-    let(:different_user_tag) { create(:tag, user: different_user) }
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:user2_tag) { create(:tag, user: user2) }
 
     before :each do
-      sign_in user
-      different_user_tag
+      user2_tag
+      sign_in(user1)
     end
 
-    it "denies access to tags#show" do
-      get tag_path(different_user_tag.id)
+    it "denies access to tags#edit" do
+      get edit_tag_path(user2_tag.id)
 
+      expect(response).to_not be_successful
       expect(response).to have_http_status(302)
-      expect(response).to redirect_to root_path
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "denies access to tags#update" do
+      new_name = "completely different name"
+      patch tag_path(user2_tag, tag: {name: new_name})
+
+      expect(response).to_not be_successful
+      expect(response).to redirect_to root_url
+    end
+
+    it "denies access to tags#destroy" do
+      delete tag_path(user2_tag)
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to root_url
     end
   end
 end
