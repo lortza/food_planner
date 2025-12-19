597c5ca9c2c2df39e060368044300973d0ce07c5
Anne Richardson <richardson.ae@gmail.com>
2025-12-19 13:30:25 -0500

    Make favorite star yellow

--- app/components/material_icon.rb
+++ app/components/material_icon.rb
@@ -49,132 +49,154 @@ class MaterialIcon
   def add_shopping_cart
     content_tag(:span, "add_shopping_cart",
       class: symbol_classes,
+      alt: @title.presence || "Add to cart",
       title: @title.presence || "Add to cart")
   end
 
   def arrow_left
     content_tag(:span, "arrow_back",
       class: "#{symbol_classes} nav-link-icon left",
+      alt: @title.presence || "Back",
       title: @title.presence || "Back")
   end
 
   def archived
     content_tag(:span, "inventory_2",
       class: symbol_classes,
+      alt: @title.presence || "Archived",
       title: @title.presence || "Archived")
   end
 
   def book
     content_tag(:span, "book",
       class: symbol_classes,
+      alt: @title.presence || "Bookmarked",
       title: @title.presence || "Bookmarked")
   end
 
   def calendar_clock
     content_tag(:span, "calendar_clock",
       class: "#{symbol_classes} text-warning",
+      alt: @title.presence || "It's been a while",
       title: @title.presence || "It's been a while")
   end
 
   def clock
     content_tag(:span, "update",
       class: symbol_classes,
+      alt: @title.presence || "Duration warning",
       title: @title.presence || "Duration warning")
   end
 
   def checkmark
     content_tag(:span, "done",
       class: symbol_classes,
+      alt: @title.presence || "Done",
       title: @title.presence || "Done")
   end
 
   def convert
     content_tag(:span, "convert_to_text",
       class: symbol_classes,
+      alt: @title.presence || "Convert",
       title: @title.presence || "Convert")
   end
 
   def copy
     content_tag(:span, "file_copy",
       class: symbol_classes,
+      alt: @title.presence || "Copy",
       title: @title.presence || "Copy")
   end
 
   def edit_note
     content_tag(:span, "edit_note",
       class: "#{symbol_classes} float-right",
+      alt: @title.presence || "Edit",
       title: @title.presence || "Edit")
   end
 
   def event_repeat
     content_tag(:span, "event_repeat",
       class: symbol_classes,
+      alt: @title.presence || "Repeated",
       title: @title.presence || "Repeated")
   end
 
   def info
     content_tag(:span, "info",
       class: symbol_classes,
+      alt: @title.presence || "Info",
       title: @title.presence || "Info")
   end
 
   def new_release
     content_tag(:span, "new_releases",
       class: symbol_classes,
+      alt: @title.presence || "New",
       title: @title.presence || "New")
   end
 
   def plus_circle
     content_tag(:span, "add_circle",
       class: symbol_classes,
+      alt: @title.presence || "Add",
       title: @title.presence || "Add")
   end
 
   def open_in_new
     content_tag(:span, "open_in_new",
       class: symbol_classes,
+      alt: @title.presence || "Open",
       title: @title.presence || "Open")
   end
 
   def plus_square
     content_tag(:span, "add_box",
       class: symbol_classes,
+      alt: @title.presence || "Add to list",
       title: @title.presence || "Add to list")
   end
 
   def quick_reference
     content_tag(:span, "quick_reference",
       class: symbol_classes,
+      alt: @title.presence || "Learn more",
       title: @title.presence || "Learn more")
   end
 
   def shopping_bag
     content_tag(:span, "shopping_bag",
       class: icon_classes,
+      alt: @title.presence || "In shopping bag",
       title: @title.presence || "In shopping bag")
   end
 
   def shopping_cart
     content_tag(:span, "shopping_cart",
       class: icon_classes,
+      alt: @title.presence || "Cart",
       title: @title.presence || "Cart")
   end
 
   def star
     content_tag(:span, "star",
       class: symbol_classes,
+      alt: @title.presence || "Star",
       title: @title.presence || "Star")
   end
 
   def settings
     content_tag(:span, "settings",
       class: symbol_classes,
+      alt: @title.presence || "Settings",
       title: @title.presence || "Settings")
   end
 
   def search
     content_tag(:span, "search",
       class: symbol_classes,
+      alt: @title.presence || "Search",
       title: @title.presence || "Search",
       aria: {hidden: true})
   end
@@ -182,18 +204,21 @@ class MaterialIcon
   def sync
     content_tag(:span, "sync",
       class: symbol_classes,
+      alt: @title.presence || "Sync",
       title: @title.presence || "Sync")
   end
 
   def trash
     content_tag(:span, "delete",
       class: symbol_classes,
+      alt: @title.presence || "Delete",
       title: @title.presence || "Delete")
   end
 
   def truck
     content_tag(:span, "local_shipping",
       class: symbol_classes,
+      alt: @title.presence || "Delivery",
       title: @title.presence || "Delivery")
   end
 
