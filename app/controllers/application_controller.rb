5ec4edcca0d238131e8ac57ba8d2bfedead00068
Anne Richardson <richardson.ae@gmail.com>
2026-03-16 11:56:32 -0400

    Reconcile with new app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+
   include Pundit::Authorization
 
   protect_from_forgery with: :exception
