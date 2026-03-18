028bb37851ba6a193f11e72953d29f2cec092e6c
Anne Richardson <richardson.ae@gmail.com>
2026-03-18 13:47:35 -0400

    Update sentry configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < ActionController::Base
 
   # New Configs: https://docs.sentry.io/platforms/ruby/migration/
   def set_sentry_context
-    Sentry.set_user(id: session[:current_user_id]) # or anything else in session
+    Sentry.set_user(id: current_user&.id)
     Sentry.set_extras(params: params.to_unsafe_h, url: request.url)
   end
 end
