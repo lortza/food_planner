f1c1e065e1cc42bde941d0312fe52dd54024935f
Anne Richardson <richardson.ae@gmail.com>
2026-03-28 15:22:55 -0400

    Make stimulus controller more generic

--- app/javascript/controllers/instructions_controller.js
+++
@@ -1,6 +0,0 @@
-import { Controller } from "@hotwired/stimulus"                                                                                                                                                                    
-export default class extends Controller {
-  toggle(event) {
-    event.target.closest("li")?.classList.toggle("line-through")
-  }
-}
