e781b226fa7232b402ad5289d54ef900dda0a5e6
Anne Richardson <richardson.ae@gmail.com>
2026-03-28 16:48:21 -0400

    comment out old data migration

--- db/migrate/20260326014339_backfill_last_prepared_on_recipe_data.rb
+++ db/migrate/20260326014339_backfill_last_prepared_on_recipe_data.rb
@@ -1,16 +1,18 @@
-class BackfillLastPreparedOnRecipeData < ActiveRecord::Migration[8.1]
-  def up
-    say_with_time "Backfilling last_prepared_on for recipes" do
-      Recipe.find_each do |recipe|
-        last_prepared_on = recipe.meal_plans.maximum(:prepared_on)
-        recipe.update_column(:last_prepared_on, last_prepared_on) unless last_prepared_on.nil?
-      end
-    end
-  end
+# Commenting out this migration since it was only needed for backfilling data and is no longer necessary. 
 
-  def down
-    say_with_time "Removing last_prepared_on from recipes" do
-      Recipe.update_all(last_prepared_on: nil)
-    end
-  end
-end
+# class BackfillLastPreparedOnRecipeData < ActiveRecord::Migration[8.1]
+#   def up
+#     say_with_time "Backfilling last_prepared_on for recipes" do
+#       Recipe.find_each do |recipe|
+#         last_prepared_on = recipe.meal_plans.maximum(:prepared_on)
+#         recipe.update_column(:last_prepared_on, last_prepared_on) unless last_prepared_on.nil?
+#       end
+#     end
+#   end
+
+#   def down
+#     say_with_time "Removing last_prepared_on from recipes" do
+#       Recipe.update_all(last_prepared_on: nil)
+#     end
+#   end
+# end
