00a2aef20ed26e6f481b5380decdcc48ac30489e
richardsonae <richardson.ae@gmail.com>
2026-04-13 17:50:30 -0400

    Replace turbo stream round-trip with template + Stimulus for adding ingredient fields (#1273)
    * Remove turbo stream approach logic
    
    * Update readme
    
    * Use template and stimulus to add/remove ingredient fields
    
    * Nil out the quantity field for new fields
    
    * Rename add to append for clarity

---
+++ app/javascript/controllers/add_ingredient_fields_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["template", "container"]
+  
+  append(event) {
+    event.preventDefault()
+    const content = this.templateTarget.content.cloneNode(true)                                                                                                       
+    const index = Date.now()
+
+    content.querySelectorAll('[name*="NEW_RECORD"], [id*="NEW_RECORD"]').forEach(el => {
+      if (el.name) el.name = el.name.replace('NEW_RECORD', index)
+      if (el.id) el.id = el.id.replace('NEW_RECORD', index)
+    })
+
+    this.containerTarget.appendChild(content)
+  } 
+  
+  remove(event) {                                                                                                                                                     
+    event.preventDefault()                                                                                                                                            
+    event.target.closest('[data-add-ingredient-fields-target="ingredientFieldsRow"]').remove()                                                                                   
+  } 
+}
